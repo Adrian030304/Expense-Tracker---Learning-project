19670da7afe9e2f17d5d7d101922ecc71981b122
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-13 20:48:16 +0300

    Declare js variables related to html

---
+++ main.js
@@ -0,0 +1,12 @@
+const incomeInput = document.getElementById("income-description");
+const incomeAmountInput = document.getElementById("income-amount");
+const expenseForm = document.getElementById("expense-form");
+const expenseInput = document.getElementById("expense-description");
+const expenseAmountInput = document.getElementById("expense-amount");
+const categoryInput = document.getElementById("category-input");
+const transactionList = document.getElementById("transaction-list");
+const totalExpense = document.getElementById("total-expense");
+const totalIncome = document.getElementById("total-income");
+const balance = document.getElementById("balance");
+
+
