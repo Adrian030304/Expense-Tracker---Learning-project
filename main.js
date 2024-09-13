2c4e3279e75edad13d170367cbc7b6ab04782138
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-13 21:39:08 +0300

    created expense form and made the logic

--- main.js
+++ main.js
@@ -4,9 +4,25 @@ const expenseForm = document.getElementById("expense-form");
 const expenseInput = document.getElementById("expense-description");
 const expenseAmountInput = document.getElementById("expense-amount");
 const categoryInput = document.getElementById("expense-category");
-const transactionList = document.getElementById("transaction-list");
-const totalExpense = document.getElementById("total-expense");
+//const transactionList = document.getElementById("transaction-list");
+const totalExpense = document.getElementById("total-expenses");
 const totalIncome = document.getElementById("total-income");
 const balance = document.getElementById("balance");
 
 
+expenseForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const description = expenseInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+    const category = categoryInput.value;
+
+    if (description === '' || isNaN(amount) || amount <= 0) {
+        alert('Please enter a valid expense description and amount.');
+        return;
+    }
+
+
+
+    event.target.submit()
+})
